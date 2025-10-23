bbf4072ffbe1c0ef86ec825e08d6c09fb209e9cc
Ajmal <Ajmal@DESKTOP-01V489G>
2025-10-22 23:23:55 -0700

    u

--- Tyuiu.ShahabAJ.Sprint1.Task0.V28.Lib/DataService.cs
+++ Tyuiu.ShahabAJ.Sprint1.Task0.V28.Lib/DataService.cs
@@ -4,7 +4,6 @@ namespace Tyuiu.ShahabAJ.Sprint1.Task0.V28.Lib
 {
     public class DataService : ISprint1Task0V28
     {
-    
             public double Calculate()
         {
             return 48.0 / 12.0 - 48.0 / 6.0 / 4.0;
