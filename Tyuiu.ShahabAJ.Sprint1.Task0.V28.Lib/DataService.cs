450a6733b4d81e1f12abf49cf15f226cddc4613f
Ajmal <Ajmal@DESKTOP-01V489G>
2025-10-22 23:22:06 -0700

    dsd

--- Tyuiu.ShahabAJ.Sprint1.Task0.V28.Lib/DataService.cs
+++ Tyuiu.ShahabAJ.Sprint1.Task0.V28.Lib/DataService.cs
@@ -4,14 +4,11 @@ namespace Tyuiu.ShahabAJ.Sprint1.Task0.V28.Lib
 {
     public class DataService : ISprint1Task0V28
     {
-        public DataService()
-        {
-
-        }
-
-        public double Calculate()
+    
+            public double Calculate()
         {
             return 48.0 / 12.0 - 48.0 / 6.0 / 4.0;
         }
+     
     }
 }
