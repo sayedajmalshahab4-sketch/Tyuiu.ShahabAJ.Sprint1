68710db1c9ca9b29cb7cf12d6225df9ddc765832
Ajmal <Ajmal@DESKTOP-01V489G>
2025-10-24 11:03:00 -0700

    Task3

---
+++ Tyuiu.ShahabAJ.Sprint1.Task3.V6.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.ShahabAJ.Sprint1.Task3.V6.Lib;
+
+namespace Tyuiu.ShahabAJ.Sprint1.Task3.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double distance = 67;
+            double fuelConsumption = 8.5;
+            double pricePerLiter = 6.5;
+            double wait = 74.035;
+            double result = ds.TravelCost(distance, fuelConsumption, pricePerLiter);
+            Assert.AreEqual(wait, result);
+        }
+    }
+}
