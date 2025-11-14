085288c441fd1b16d375b8d64a569d1c5cfaa4e5
Ajmal <Ajmal@DESKTOP-01V489G>
2025-11-14 16:15:44 +0500

    Task5

--- Tyuiu.ShahabAJ.Sprint1.Task5.V7.Lib/DataService.cs
+++ Tyuiu.ShahabAJ.Sprint1.Task5.V7.Lib/DataService.cs
@@ -1,26 +1,18 @@
 ﻿using System;
-
+using tyuiu.cources.programming.interfaces.Sprint1;
 namespace Tyuiu.ShahabAJ.Sprint1.Task5.V7.Lib
 {
-    public class DataService
+    public class DataService : ISprint1Task5V7
     {
-        public int CalculateHours(double f)
+        public int AngleToHoursMinutes(double f)
         {
             throw new NotImplementedException();
         }
-    }
 
-    ISprint1Task6V14
-    {
         public int CalculateHours(double f)
         {
-            // Часовая стрелка проходит 360 градусов за 12 часов
-            // Значит 1 час = 30 градусов (360 / 12 = 30)
             double hours = f / 30.0;
-
-            // Берем целую часть (полные часы)
             int fullHours = (int)hours;
-
             return fullHours;
         }
     }
--- Tyuiu.ShahabAJ.Sprint1.Task5.V7.Test/DataServiceTest.cs
+++ Tyuiu.ShahabAJ.Sprint1.Task5.V7.Test/DataServiceTest.cs
@@ -7,31 +7,21 @@ namespace Tyuiu.ShahabAJ.Sprint1.Task5.V7.Test
     public class DataServiceTest
     {
         [TestMethod]
-        public void ValidAngle90()
+        public void ValidCalculateHours()
         {
             DataService ds = new DataService();
             double f = 90;
-            int wait = 3;  // 90 / 30 = 3 часа
+            int wait = 3;
             int result = ds.CalculateHours(f);
             Assert.AreEqual(wait, result);
         }
 
         [TestMethod]
-        public void ValidAngle180()
+        public void ValidCalculateHours2()
         {
             DataService ds = new DataService();
             double f = 180;
-            int wait = 6;  // 180 / 30 = 6 часов
-            int result = ds.CalculateHours(f);
-            Assert.AreEqual(wait, result);
-        }
-
-        [TestMethod]
-        public void ValidAngle45()
-        {
-            DataService ds = new DataService();
-            double f = 45;
-            int wait = 1;  // 45 / 30 = 1.5 → целая часть = 1
+            int wait = 6;
             int result = ds.CalculateHours(f);
             Assert.AreEqual(wait, result);
         }
