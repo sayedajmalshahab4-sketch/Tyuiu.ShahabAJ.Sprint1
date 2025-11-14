bed536254ed9cf1f007ff215a49b476f12a057fa
Ajmal <Ajmal@DESKTOP-01V489G>
2025-11-14 16:50:11 +0500

    Task5

--- Tyuiu.ShahabAJ.Sprint1.Task5.V7.Lib/DataService.cs
+++ Tyuiu.ShahabAJ.Sprint1.Task5.V7.Lib/DataService.cs
@@ -3,8 +3,13 @@ using tyuiu.cources.programming.interfaces.Sprint1;
 
 namespace Tyuiu.ShahabAJ.Sprint1.Task5.V0.Lib
 {
-    public class DataService :ISprint1Task5V7
+    public class DataService : ISprint1Task5V7
     {
+        public int AngleToHoursMinutes(double f)
+        {
+            throw new NotImplementedException();
+        }
+
         public double Calculate(double x)
         {
             double result = Math.Pow(x, 2) / Math.Sqrt(x);
